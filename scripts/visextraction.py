744282ff8e2b0036439c26f725ed0064669e0717
aarushiwagh <aarushiwagh16@gmail.com>
2024-11-18 14:26:04 -0500

    changes to vision embeddings extraction

--- scripts/visextraction.py
+++ scripts/visextraction.py
@@ -9,18 +9,19 @@ from tqdm import tqdm
 
 from visual_embeddings import list_visual_files_paths, extract_and_save_visual_embeddings
 
-csv_path = "/home/hice1/awagh31/scratch/original/labels/labels/detailed_lables.csv"
+# path to the detailed_labels.csv file from the edaic labels folder
+csv_path = "/home/hice1/awagh31/scratch/original/labels/labels/detailed_lables.csv" 
 labels_df = pd.read_csv(csv_path)
 
-# Define directory paths and columns for metadata
-main_dir = "/home/hice1/awagh31/scratch/original/data_untarred" #folder which contains each patient's data
+# folder which contains each patient's data
+main_dir = "/home/hice1/awagh31/scratch/original/data_untarred" 
 cols = ["gender", "split", "PTSD_label", "age", "PTSD_severity"]
 
 # List visual file paths with metadata
 visual_paths_df = list_visual_files_paths(main_dir, labels_df, cols)
 #print(visual_paths_df.head)
 
-# Save visual file paths and metadata to CSV for reference
+# the directory where we want to save paths and metadata to CSV for reference
 save_dir = '/home/hice1/awagh31/scratch/scripts'
 os.makedirs(save_dir, exist_ok=True)
 csv_path = os.path.join(save_dir, "labels_visual_files_dn.csv")
@@ -28,7 +29,7 @@ csv_path = os.path.join(save_dir, "labels_visual_files_dn.csv")
 file_name = "labels_visual_files_dn.csv"
 visual_paths_df.to_csv(os.path.join(csv_path), index=False)
 
-# Process and save visual embeddings
+# path to directory for final embeddings
 target_path = "/home/hice1/awagh31/scratch/final_embeddings_dn"
 visual_file_paths = visual_paths_df["file_path"]
 
